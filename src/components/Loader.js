9a6efeeb909f60816325a322d38334337e8bae7d
Ade-yanju <adeyanjuolamilekan080@gmail.com>
2026-02-14 07:50:20 +0100

    final

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,35 +1,39 @@
+import logo from "../assets/logo.png";
+
 export default function Loader() {
-    return (
-      <div style={{
+  return (
+    <div
+      style={{
         height: "100vh",
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
         flexDirection: "column",
-        background: "#fff"
-      }}>
-        
-        <img src="/logo.png" alt="logo" style={{ height: "60px" }} />
-  
-        <div style={{
+        background: "#fff",
+      }}
+    >
+      <img src={logo} alt="logo" style={{ height: "60px" }} />
+
+      <div
+        style={{
           marginTop: "20px",
           width: "40px",
           height: "40px",
           border: "4px solid #0B5ED7",
           borderTop: "4px solid transparent",
           borderRadius: "50%",
-          animation: "spin 1s linear infinite"
-        }} />
-  
-        <style>
-          {`
-            @keyframes spin {
-              0% { transform: rotate(0deg); }
-              100% { transform: rotate(360deg); }
-            }
-          `}
-        </style>
-      </div>
-    );
-  }
-  
+          animation: "spin 1s linear infinite",
+        }}
+      />
+
+      <style>
+        {`
+          @keyframes spin {
+            0% { transform: rotate(0deg); }
+            100% { transform: rotate(360deg); }
+          }
+        `}
+      </style>
+    </div>
+  );
+}
